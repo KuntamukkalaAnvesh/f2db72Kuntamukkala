c68eff9c9acf43d86b4d596dc82f4111ad1bdd02
KuntamukkalaAnvesh <112181431+KuntamukkalaAnvesh@users.noreply.github.com>
2022-11-16 23:40:25 -0600

    updated

--- controllers/Bear.js
+++ controllers/Bear.js
@@ -99,3 +99,67 @@ exports.Bear_create_post = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
 }
+// Handle Bear delete on DELETE.
+exports.Bear_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await Bear.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+    };
+// Handle a show one view with id specified by query
+exports.Bear_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await Bear.findById( req.query.id)
+    res.render('Beardetail',
+    { title: 'Bear Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+// Handle building the view for creating a Bear.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.Bear_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('Bearcreate', { title: 'Bear Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+// Handle building the view for updating a Bear.
+// query provides the id
+exports.Bear_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await Bear.findById(req.query.id)
+    res.render('Bearupdate', { title: 'Bear Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+// Handle a delete one view with id from query
+exports.Bear_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await Bear.findById(req.query.id)
+    res.render('Beardelete', { title: 'Bear Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
--- routes/Bear.js
+++ routes/Bear.js
@@ -5,3 +5,13 @@ var router = express.Router();
 router.get('/', Bear_controlers.Bear_view_all_Page );
 router.get('/bear/:id', Bear_controlers.Bear_detail); 
 module.exports = router;
+// GET request for one Bear.
+router.get('/Bear/:id', Bear_controlers.Bear_detail);
+/* GET detail Bear page */
+router.get('/detail', Bear_controlers.Bear_view_one_Page);
+/* GET create Bear page */
+router.get('/create', Bear_controlers.Bear_create_Page);
+/* GET create update page */
+router.get('/update', Bear_controlers.Bear_update_Page);
+/* GET delete Bear page */
+router.get('/delete', Bear_controlers.Bear_delete_Page);
